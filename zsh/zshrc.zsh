36805dedb3f0f555695add6259b5a5c2e41c5dc0
Christopher Garrett <me@pzuraq.com>
2017-04-01 17:43:32 -0700

    add glog alias

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,4 +1,6 @@
 source ~/.dotfiles/zsh/zplug.zsh
 
+alias glog="git log --pretty='format:%d %Cgreen%h%Creset %an - %s' --graph"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
