1cc804102111643c6848b70a9fcdf6f2b5099767
Christopher Garrett <me@pzuraq.com>
2018-12-02 18:16:46 -0800

    updates karabiner, adds scripts

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -8,22 +8,11 @@ alias gdel="git branch -D"
 alias glog="git log --pretty='format:%d %Cgreen%h%Creset %an - %s' --graph"
 
 alias git-clean-local="git branch -r | sed -n -e 's/origin\///p' | awk '{ print $1 }' | cat < <(git rev-parse --abbrev-ref HEAD) | egrep -v -f /dev/fd/0 <(git branch) | awk '{print $1}' | xargs git branch -D"
-alias load_db='./Tools/scripts/fetch_remote_db.sh --ldap-username chris.garrett --fetch-db --no-backup-db'
-
-# ulimit -n 65536 65536
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-# Seems to fix VSCode issue for now
-
-# source ~/.zshrc_local
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# eval "$(rbenv init -)"
 
 export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$PATH:$HOME/.dotfiles/scripts"
 
-# tabtab source for electron-forge package
-# uninstall by removing these lines or running `tabtab uninstall electron-forge`
-[[ -f /Users/pzuraq/Code/pzuraq/tewdew/node_modules/tabtab/.completions/electron-forge.zsh ]] && . /Users/pzuraq/Code/pzuraq/tewdew/node_modules/tabtab/.completions/electron-forge.zsh
+export NOTION_HOME="$HOME/.notion"
+[ -s "$NOTION_HOME/load.sh" ] && \. "$NOTION_HOME/load.sh"
+
+export PATH="${NOTION_HOME}/bin:$PATH"
