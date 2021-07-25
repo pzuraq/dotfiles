a9d371a9f6ad37aaa004ed4f84fc2f6d05485f04
Chris Garrett <me@pzuraq.com>
2021-07-25 17:07:18 -0400

    Update zshrc.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,20 +1,9 @@
-source ~/.dotfiles/zsh/zplug.zsh
-
-alias gci="git commit"
-alias gco="git checkout"
-alias gbr="git branch"
-alias gst="git status"
-alias gdel="git branch -D"
-alias glog="git log --pretty='format:%d %Cgreen%h%Creset %an - %s' --graph"
-
 alias git-clean-local="git branch -r | sed -n -e 's/origin\///p' | awk '{ print $1 }' | cat < <(git rev-parse --abbrev-ref HEAD) | egrep -v -f /dev/fd/0 <(git branch) | awk '{print $1}' | xargs git branch -D"
 
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$PATH:$HOME/.dotfiles/scripts"
 
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
-alias chrome="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
 
 export NODE_OPTIONS="--max-old-space-size=8192"
 
