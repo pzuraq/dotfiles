6b67c409480177bd22104d5c659838434136c85a
Chris Garrett <me@pzuraq.com>
2021-07-25 17:09:16 -0400

    Update zshrc.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,10 @@
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+# Customize to your needs...
+
 alias git-clean-local="git branch -r | sed -n -e 's/origin\///p' | awk '{ print $1 }' | cat < <(git rev-parse --abbrev-ref HEAD) | egrep -v -f /dev/fd/0 <(git branch) | awk '{print $1}' | xargs git branch -D"
 
 export PATH="$HOME/.cargo/bin:$PATH"
