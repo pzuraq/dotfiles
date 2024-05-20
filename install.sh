947ab3ac584230a4de311f3838c4f1773535337d
Kristen Maevyn <kris@bitski.com>
2024-05-20 15:37:01 -0400

    add clone

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+cd $HOME
+mkdir -p "$HOME/Code"
 
 dotfiles="$HOME/.dotfiles"
+
+xcode-select --install
+
+if [ ! -d $dotfiles ]
+then
+  git clone https://github.com/pzuraq/dotfiles.git .dotfiles
+fi
 
 link() {
   from="$dotfiles/$1"
@@ -22,3 +31,5 @@ link "vscode/settings.json" "Library/Application Support/Code/User/settings.json
 link "vscode/keybindings.json" "Library/Application Support/Code/User/keybindings.json"
 
 link "zsh/zshrc.zsh" ".zshrc"
+
+exit 0
